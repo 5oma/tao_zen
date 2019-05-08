0ebca07166444944d0958331f820845cb8cc6f5c
5oma <ariel.carbajal.contacto@gmail.com>
2019-05-07 20:29:17 -0500

    Añadiendo algunas líneas.

--- tz_quotes_dict.py
+++ tz_quotes_dict.py
@@ -3,9 +3,18 @@
 import random
 
 dict_lineas_tz = {
-    "No hay acción indigna.": "linea0", 
-    "Acarrea agua y recoge leña": "linea1",
-    "Simplemente empieza.": "linea2"
+    "No hay acción indigna.": "0", 
+    "Acarrea agua y recoge leña": "1",
+    "Simplemente empieza.": "2",
+    "Wu-wei, sin esfuerzo.": "3",
+    "Tzu-jan, por sí mismo.": "4",
+    "Momento a momento.": "5",
+    "Transmisión de praxis, no dogma.": "6",
+    "Partir desde la más sencilla tarea": "7",
+    "Deja que todo sea lo que es.": "8",
+    "Actividad maravillosa.": "9",
+    "En el más mínimo gesto.": "10",
+ 
 }
 
 
