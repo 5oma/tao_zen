85cd6fc49546a86c5b554f559fb124a1be65da26
5oma <ariel.carbajal.contacto@gmail.com>
2019-05-07 20:16:04 -0500

    Añadiendo una máquina de frases zen.

---
+++ tz_quotes_dict.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import random
+
+dict_lineas_tz = {
+    "No hay acción indigna.": "linea0", 
+    "Acarrea agua y recoge leña": "linea1",
+    "Simplemente empieza.": "linea2"
+}
+
+
+print(random.choice(list(dict_lineas_tz)))
+
+
+
+
